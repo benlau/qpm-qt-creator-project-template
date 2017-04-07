fe6c15c9d7781278becd6180b286020aba323744
Ben Lau <xbenlau@gmail.com>
2017-04-07 09:51:13 +0800

    Upgrade testable version

--- Testable Project/tests.cpp
+++ Testable Project/tests.cpp
@@ -6,7 +6,7 @@
 Tests::Tests(QObject *parent) : QObject(parent)
 {
 
-    // This function do nothing but could make Qt Creator Autotests plugin recognize QuickTests
+    // This function do nothing but could make Qt Creator Autotests plugin recognize this unit test
     auto ref =[=]() {
         QTest::qExec(this, 0, 0);
     };
